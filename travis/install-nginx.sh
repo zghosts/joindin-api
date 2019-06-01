499e31653141a763988b86cab88cf572a084aca5
Jos Elstgeest <zawadi@gmail.com>
2019-06-02 00:46:01 +0200

    stop nginx before starting

--- travis/install-nginx.sh
+++ travis/install-nginx.sh
@@ -44,4 +44,5 @@ tpl "$DIR/fastcgi.tpl.conf" "$DIR/nginx/fastcgi.conf"
 tpl "$DIR/default-site.tpl.conf" "$DIR/nginx/sites-enabled/default-site.conf"
 
 # Start nginx.
+sudo service nginx stop
 nginx -c "$DIR/nginx/nginx.conf"
